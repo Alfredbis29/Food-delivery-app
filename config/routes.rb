ba482cfbf3079243d324ee68e3c7e018f37ffc5b
vic778 <victoiremmanuelbarh@gmail.com>
2022-11-07 14:31:31 +0400

    Generate the homes page and make it as the root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get 'homes/index'
+  root 'homes#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  resources :foods
+
 end
