8a53d36bd852a4bbe47b955e6dead5b38c904d40
vic778 <victoiremmanuelbarh@gmail.com>
2022-11-07 14:25:39 +0400

    Generate moigration roel to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_07_101014) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_07_102358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.text "descritpion"
+    t.string "image"
+    t.float "price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -27,9 +38,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_07_101014) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "role", default: false, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "foods", "users"
 end
