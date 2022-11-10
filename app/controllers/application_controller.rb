38d10e1423bbeaf24bad7d4688ed02b3ebf20a63
vic778 <victoiremmanuelbarh@gmail.com>
2022-11-10 23:16:01 +0400

    Render the error message if a user is not authorize to perfrom an action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
+
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
   
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :role])
+    end
+
+    def user_not_authorized
+      flash[:alert] = "You are not authorized to perform this action."
+        # redirect_to root_path
     end
 end
