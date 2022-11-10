e9eac3d3fff18db2624087920b546057d51779b7
vic778 <victoiremmanuelbarh@gmail.com>
2022-11-10 23:15:18 +0400

    Authorize the user

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,13 @@
 class DashboardsController < ApplicationController
   before_action :authenticate_user!
+  # before_action :set_user_autorization
   def index
+    # authorize user
+  end
+
+  private 
+
+  def set_user_autorization
+  
   end
 end
