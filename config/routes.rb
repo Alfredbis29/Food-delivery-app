895fa4d09ba8568c12b04eea8f61d8371253f70c
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2023-01-03 12:10:50 +0200

    create food

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # get 'dashboards/index'
   root 'dashboards#index'
-  # resources :foods
 
   devise_for :users
   resources :foods, only: %i[index show new create destroy]
