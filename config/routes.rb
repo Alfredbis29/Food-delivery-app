61e3a898cfe6a244684fafd3c9f27932977124a6
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2023-01-05 12:16:13 +0200

    Once the user click on the food title it display the details about that particular item

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # get 'dashboards/index'
   root 'dashboards#index'
 
+get '/food/:id', to: 'food#show'
+
   devise_for :users
   resources :foods, only: %i[index show new create destroy]
 end
