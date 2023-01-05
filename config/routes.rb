5d7565e351bfa75173dea6f7fd78f254258bba73
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2023-01-05 13:01:15 +0200

    remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   # get 'dashboards/index'
   root 'dashboards#index'
 
-get '/food/:id', to: 'food#show'
-
   devise_for :users
   resources :foods, only: %i[index show new create destroy]
 end
