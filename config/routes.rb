17872b8143821b8a70caf4e3648e98a54baebd5c
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2023-01-17 12:05:10 +0200

    upgrade menu item

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # get 'dashboards/index'
   root 'dashboards#index'
-  # resources :orders
+  resources :menu_items
 
   devise_for :users
   resources :foods, only: %i[index show new create destroy]
