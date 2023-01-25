8710eedff03ec55d750e91a9456ef3a668de4544
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2023-01-25 23:04:23 +0200

    create a new controller for handling orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
-  belongs_to :user
-  belongs_to :food
+  validates :quantity, numericality: { greater_than: 0 }
+  before_save :update_total_amount
+  def update_total_amount
+    self.total_amount = item_price * quantity
+  end
 end
