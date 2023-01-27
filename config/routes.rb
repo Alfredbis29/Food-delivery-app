e7ad69e6863e84aecdd62a7eee3c572b1d6918ea
Alfredbis29 <Kalumunabisimwa5@gmail.com>
2023-01-27 12:48:16 +0200

    add method for creating order

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # get 'dashboards/index'
   root 'foods#index'
-
+  get '/orders/new', to: 'orders#index'
   devise_for :users
   resources :foods do
     resources :orders
